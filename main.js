425586aec9b0bcf00730178ffb6c521cb7a4317b
erik-montee <erik.montee@gmail.com>
2021-06-09 22:56:29 -0400

    data.json file
    more flexibility with different accounts and different products per thread

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const {Worker} = require('worker_threads');
+const fs = require('fs');
 
 (async function example() {
-    const username = process.env.BB_USER_NAME;
-    const password = process.env.PASSWORD;
-    const cvv = process.env.CVV;
-    const numberOfThreads = process.env.NUM_THREADS;
-
-    for (let i = 0; i < numberOfThreads; i++) {
-        const originalArray = [username, password, cvv];
-        new Worker('./worker.js', {
+    let data = JSON.parse(fs.readFileSync('data.json'));
+    count = 0;
+    data.forEach(function(value) {
+        const originalArray = [value.username, value.password, value.cvv, value.url];
+        let worker = new Worker('./worker.js', {
             workerData: originalArray
         });
-    }
+
+        worker.on('message', (result) => {
+            if(result){
+                count++;
+                console.log(count);
+                console.log(process.env.NUMBER_TO_BUY);
+                console.log(count >= parseInt(process.env.NUMBER_TO_BUY));
+                if(count >= process.env.NUMBER_TO_BUY) {
+                    console.log('dieing');
+                    process.exit();
+                }
+            }
+        });
+    });
 })();
--- worker.js
+++ worker.js
@@ -7,18 +7,10 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
         const username = workerData[0];
         const password = workerData[1];
         const cvv = workerData[2];
-
         const login = process.env.LOGIN_URL;
-        const url = process.env.TARGET_URL;
+        const url = workerData[3];
         const checkout = process.env.CHECKOUT_URL;
 
-        // const login = 'https://www.bestbuy.com/identity/signin?token=tid%3Ae4470626-c8ce-11eb-b407-0a588ff6653d';
-        // const url = 'https://www.bestbuy.com/site/wowwee-fingerlings-baby-penguin-tux-multi/6361538.p?skuId=6361538';
-        // const checkout = 'https://www.bestbuy.com/checkout/r/fast-track';
-
-        //let url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789';
-        //let url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440';
-  
         await driver.get(login);
         //enter username and password
         await driver.wait(until.elementLocated(By.name('fld-e'))).sendKeys(username);
@@ -47,9 +39,11 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
                 await driver.wait(until.elementLocated(By.xpath("//input[contains (@id, 'cvv')]"))).sendKeys(cvv);
             }
      
-            //click place Order
+            //click place Order UNCOMMENT THIS LINE for actual use
             //await driver.wait(until.elementLocated(By.css("button[class='btn btn-lg btn-block btn-primary button__fast-track']"))).click();
-            setTimeout(function(){ console.log("Hello"); }, 12000);
+            parentPort.postMessage(true);
+            setTimeout(function(){ console.log("Hello"); }, 3000);
+            console.log("hiiii");
             }, 6000);
     } finally {
       //await driver.quit();
